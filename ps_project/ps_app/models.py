b39cafcda0de3b93c4a2e82e75588fbef4e21faa
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-22 19:36:20 +0545

    Models Updated

--- ps_project/ps_app/models.py
+++ ps_project/ps_app/models.py
@@ -7,8 +7,6 @@ class User(AbstractUser):
     phone = models.CharField(max_length=20, blank=True)
     address = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='images/user/', blank=True, null=True)
-    auth_token = models.CharField(max_length=255, blank=True, null=True)
-    is_verified = models.BooleanField(default=False)
     
     def __str__(self):
         return self.username
@@ -16,7 +14,6 @@ class User(AbstractUser):
 class Brand(models.Model):
     name = models.CharField(max_length=255, unique=True)
     logo = models.ImageField(upload_to='images/brands/')
-    description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
