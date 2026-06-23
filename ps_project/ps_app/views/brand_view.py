d8ad245fe91cc7e6d03c9f0eb5f75f81df42a6b2
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-23 07:28:26 +0545

    Add Brand Page Added

--- ps_project/ps_app/views/brand_view.py
+++ ps_project/ps_app/views/brand_view.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from ..models import Brand
 
 @login_required
 def add_brand_view(request):
+    errors = {}
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        logo = request.FILES.get('logo')
+        
+        if not name:
+            errors['name'] = 'Brand name is required.'
+            
+        if not logo:
+            errors['logo'] = 'Brand logo is required.'
+            
+        if errors:
+            return render(request, 'main/add_brands_page.html', {'data':request.POST, 'errors': errors})
+        
+        brand = Brand(name=name, logo=logo)
+        brand.save()
+        
+        messages.success(request, 'Brand added successfully.')
+        return redirect('/dashboard/admin/?section=brand-management')
+        
+        
+        
+        
     return render(request, 'main/add_brands_page.html')
