c4160e047077893cfef67290f3e54354ce673243
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-24 06:16:02 +0545

    Add Products Page Setup Done

---
+++ ps_project/ps_app/views/product_view.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+@login_required
+def add_product_view(request):
+    return render(request, 'main/add_products_page.html')
