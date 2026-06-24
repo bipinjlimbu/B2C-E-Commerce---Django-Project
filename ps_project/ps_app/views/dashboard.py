6d46c3d6c92c77e807589b24f4ee0e56e9373875
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-24 08:00:22 +0545

    Products Data Fetched in Admin Dashboard

--- ps_project/ps_app/views/dashboard.py
+++ ps_project/ps_app/views/dashboard.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from ..models import User, Brand
+from ..models import User, Brand,Product
 
 @login_required
 def admin_dashboard_view(request):
@@ -19,7 +19,7 @@ def admin_dashboard_view(request):
         context['customers'] = User.objects.filter(is_staff=False).order_by('-date_joined')
         
     elif section == 'product-management':
-        context['products'] = None
+        context['products'] = Product.objects.all().order_by('-created_at')
         
     elif section == 'brand-management':
         context['brands'] = Brand.objects.all()
