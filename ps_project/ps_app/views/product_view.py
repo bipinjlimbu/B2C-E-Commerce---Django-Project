6257abddf09e3d83ae9a1c1fbcb27b23d371d422
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-24 06:27:40 +0545

    Brands Fetched in Products Form

--- ps_project/ps_app/views/product_view.py
+++ ps_project/ps_app/views/product_view.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from ..models import Brand, Product
 
 @login_required
 def add_product_view(request):
-    return render(request, 'main/add_products_page.html')
+    brands = Brand.objects.all()
+    return render(request, 'main/add_products_page.html',{'brands':brands})
