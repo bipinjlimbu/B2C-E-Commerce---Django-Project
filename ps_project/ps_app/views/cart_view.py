8b71d8c631311b7972dd6a7c9d98ceff8f79d85a
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-25 08:19:23 +0545

    Cart Page Added

--- ps_project/ps_app/views/cart_view.py
+++ ps_project/ps_app/views/cart_view.py
@@ -10,3 +10,10 @@ def add_to_cart_view(request, product_id):
     cart_item, item_created = CartItem.objects.get_or_create(cart=cart, product=product)
     messages.success(request, "Product added to cart.")
     return redirect('/products')
+
+@login_required
+def cart_view(request):
+    cart, created = Cart.objects.get_or_create(customer=request.user)
+    cart_items = cart.items.all()
+    total_price = sum(item.total_price for item in cart_items)
+    return render(request, 'main/cart_page.html', {'cart_items': cart_items, 'total_price': total_price})
