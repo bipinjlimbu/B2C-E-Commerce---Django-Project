6fe88bcac48d26b0aed56141f309f5db1d1b8244
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-25 08:00:02 +0545

    Cart and Cart Item Seperated

--- ps_project/ps_app/models.py
+++ ps_project/ps_app/models.py
@@ -65,8 +65,15 @@ class Wishlist(models.Model):
 
 class Cart(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
 
 class Order(models.Model):
     class Status(models.TextChoices):
