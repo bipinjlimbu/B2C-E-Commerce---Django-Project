853fb07a4c44e076f2309c29d26c45e27ab3a9a6
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-25 08:02:35 +0545

    Add to Cart Logic Updated

--- ps_project/ps_app/views/cart_view.py
+++ ps_project/ps_app/views/cart_view.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from ..models import Cart, Product
+from ..models import Cart, Product, CartItem
 
 @login_required
 def add_to_cart_view(request, product_id):
     product = Product.objects.get(id=product_id)
-    cart, created = Cart.objects.get_or_create(customer=request.user, product = product )
+    cart, created = Cart.objects.get_or_create(customer=request.user)
+    cart_item, item_created = CartItem.objects.get_or_create(cart=cart, product=product)
     messages.success(request, "Product added to cart.")
     return redirect('/products')
