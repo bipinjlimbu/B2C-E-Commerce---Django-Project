06c8c75a33868e22acdf6b52870ae7eb63d81e9b
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-28 19:04:42 +0545

    My Orders Section Data Fetched in Backend

--- ps_project/ps_app/views/dashboard.py
+++ ps_project/ps_app/views/dashboard.py
@@ -50,7 +50,7 @@ def customer_dashboard_view(request):
         context['pending_orders'] = Order.objects.filter(customer=request.user).exclude(status__in=[Order.Status.COMPLETED, Order.Status.CANCELLED]).order_by('-created_at')
         
     elif section == 'my-orders':
-        context['orders'] = None
+        context['orders'] = Order.objects.filter(customer=request.user, status__in=[Order.Status.COMPLETED, Order.Status.CANCELLED]).order_by('-created_at')
         
     elif section == 'total-spent':
         context['total_spent'] = None
