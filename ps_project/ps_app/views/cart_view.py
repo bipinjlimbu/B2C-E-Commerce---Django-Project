c1be0e1719fb5d57d4121c08207c60dcf5b5a49d
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-18 09:45:56 +0545

    Fixes Done

--- ps_project/ps_app/views/cart_view.py
+++ ps_project/ps_app/views/cart_view.py
@@ -6,6 +6,11 @@ from ..models import Cart, Product, CartItem
 @login_required
 def add_to_cart_view(request, product_id):
     product = Product.objects.get(id=product_id)
+    
+    if product.stock <= 0:
+        messages.warning(request, "This product is out of stock.")
+        return redirect('/products')
+    
     cart, created = Cart.objects.get_or_create(customer=request.user)
     cart_item, item_created = CartItem.objects.get_or_create(cart=cart, product=product)
     messages.success(request, "Product added to cart.")
